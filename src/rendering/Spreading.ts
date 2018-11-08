5e6a18d95ed3253c51ae63605486f4e01106f133
Alex Burner <alex.burner@gmail.com>
2018-11-07 20:13:52 -0800

    Restore long plane

--- src/rendering/Spreading.ts
+++ src/rendering/Spreading.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 
 import { SimulationData3 } from '../simulation/Simulation'
-import { makeSquarePlane } from './layers/plane'
+import { makeLongPlane, makeSquarePlane } from './layers/plane'
 import { Points } from './layers/Points'
 import { TimeTrails } from './layers/TimeTrails'
 import { SpaceArgs } from './Row'
@@ -15,14 +15,14 @@ export class Spreading {
 
   constructor({ radius, x, y, z }: SpaceArgs) {
     const squarePlane = makeSquarePlane(radius)
-    // const longPlane = makeLongPlane(radius + 2, 100)
-    // longPlane.position.z -= 100
-    // longPlane.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2)
+    const longPlane = makeLongPlane(radius, 100)
+    longPlane.position.z -= 100
+    longPlane.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2)
     this.points = new Points()
     this.timeTrails = new TimeTrails()
     this.group = new THREE.Group()
     this.group.add(squarePlane)
-    // this.group.add(longPlane)
+    this.group.add(longPlane)
     this.group.add(this.points.getObject())
     this.group.add(this.timeTrails.getObject())
     this.group.position.set(x, y, z)
