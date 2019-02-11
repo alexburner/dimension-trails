44b9a482ea3abfb3bd88bbacfadce684dc348fcd
Alex Burner <alex.burner@gmail.com>
2019-02-10 16:30:02 -0800

    s/spec/vector/

--- src/rendering/layers/plane.ts
+++ src/rendering/layers/plane.ts
@@ -44,12 +44,12 @@ export const makeLongPlane = (radius: number, length: number): THREE.Object3D =>
     // [radius, length, 0],
   ])
 
-const makePlane = (specs: [number, number, number][]): THREE.Object3D => {
+const makePlane = (vectors: [number, number, number][]): THREE.Object3D => {
   const group = new THREE.Group()
-  times(specs.length, i => {
+  times(vectors.length, i => {
     if (i % 2) return // only do evens, each pair
-    const source = new THREE.Vector3(...specs[i])
-    const target = new THREE.Vector3(...specs[i + 1])
+    const source = new THREE.Vector3(...vectors[i])
+    const target = new THREE.Vector3(...vectors[i + 1])
     const line = new THREE.Line(new THREE.Geometry(), material)
     const geometry = line.geometry as THREE.Geometry
     line.computeLineDistances()
